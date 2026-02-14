ccd00eeb5ff377fd37fd9a0f386df74e023a29e6
Yu <yu7dev@gmail.com>
2026-02-14 08:52:26 +0300

    import vite-plugin-static-copy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy'; 
 
 export default defineConfig({
 	base: 'a-smart-fish',
