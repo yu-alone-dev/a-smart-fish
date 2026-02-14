3e4c3c134336c19966cf1d48dbecf346bbfd73df
Yu <yu7dev@gmail.com>
2026-02-14 18:04:04 +0300

    Updates

--- _app/immutable/chunks/0uNwC2O6.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1d3mcsc?.base??"",a=globalThis.__sveltekit_1d3mcsc?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/Pu_Wkduu.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_lmyrvb?.base??"",a=globalThis.__sveltekit_lmyrvb?.assets??s??"";export{a,s as b};
