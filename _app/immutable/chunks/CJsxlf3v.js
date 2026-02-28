3658bb672167bda9b53ffcac29c92b2814a72f70
Yu <yu7dev@gmail.com>
2026-03-01 01:30:03 +0300

    Updates

---
+++ _app/immutable/chunks/CJsxlf3v.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1wm4id?.base??"",a=globalThis.__sveltekit_1wm4id?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.CzJHDhhL.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CKb-TF7x.js";import{v as t}from"../chunks/BZaXEI81.js";export{t as load_css,r as start};
